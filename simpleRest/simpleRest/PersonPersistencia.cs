0a73552f767e4a0fb150fec7751c4c785d487fa5
Victor Jimenez <vtorero@gmail.com>
2018-06-27 10:13:25 -0500

    api

--- simpleRest/simpleRest/PersonPersistencia.cs
+++ simpleRest/simpleRest/PersonPersistencia.cs
@@ -2,38 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
 
 namespace simpleRest
 {
     public class PersonPersistencia
+
     {
-        private MySql.Data.MySqlClient.MySqlConnection conn;
+        private SqlConnection conn;
+        private SqlCommand command;
+
 
         public PersonPersistencia()
         {
             string myConnectionString;
-            myConnectionString = "server=127.0.0.1; uid=root; pwd=; database=test";
+
+   
+            myConnectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=inspeccion;Integrated Security=SSPI;";
             try
             {
-                conn = new MySql.Data.MySqlClient.MySqlConnection();
-                conn.ConnectionString = myConnectionString;
-                conn.Open();
-            }
-            catch (MySql.Data.MySqlClient.MySqlException ex)
+                conn = new SqlConnection(myConnectionString);
+                
+                       }
+            catch (Exception ex)
             {
 
-                throw;
+               
             }
 
         }
 
         public long savePerson(Models.Person personToSave)
         {
-            String sqlString = "INSERT INTO person (Apellidos,Nombres,StarDate,EndDate) VALUES('" + personToSave.Apellidos + "','" + personToSave.Nombres + "','" + personToSave.StartDate.ToString("yyyy-MM-dd HH:mm:ss") + "','" + personToSave.EndDate.ToString("yyyy-MM-dd HH:mm:ss") + "')";
-            MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(sqlString, conn);
-            cmd.ExecuteNonQuery();
-            long id = cmd.LastInsertedId;
-            return id;
+            String sqlString = "INSERT INTO empleado (Apellidos,Nombres) VALUES('" + personToSave.Apellidos + "','" + personToSave.Nombres + "')";
+            conn.Open();
+            command = new SqlCommand(sqlString, conn);
+            command.ExecuteNonQuery();
+
+            return 1;
 
         }
 
